102071fd77a0c6856ed105ad6e5e79a7acbd44d7
ramkumar <ramviru9@gmail.com>
2022-05-20 13:42:53 +0530

    commit

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    origin:"http://localhost:3000"
-    // origin:"https://voluble-torrone-9375e0.netlify.app"
+    origin:"http://localhost:3000,https://voluble-torrone-9375e0.netlify.app"
   })
 );
 app.use(express.json());
