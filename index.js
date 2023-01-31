67c4294f1648645d70c7956565cf13cc27eceae4
ramkumar <ramviru9@gmail.com>
2023-01-31 13:40:39 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(
   cors(
     {
-    origin:["http://localhost:3000","https://visionary-stardust-352866.netlify.app"]
+    origin:["http://localhost:3000","https://incomeexpense-tracker.netlify.app"]
   }
   )
 );
